56a1f42435831667077f65f7bc9e3138f0468629
Nick James Brown <nbrown9@uvm.edu>
2018-04-26 13:20:58 -0400

    Changed index

--- index.php
+++ index.php
@@ -1,8 +1,100 @@
-<?php
+<!DOCTYPE HTML>
+<html lang="en">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <head>
+        <title>CS142</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Nick Brown">
+        <meta name="description" content="A site map to all my assignments">
+        <link rel ="stylesheet"
+              href="sitemap.css"
+              type ="text/css"
+              media ="screen">
+    </head>
 
+    <body>
+        <figure class="right small">
+            <img src="wood.jpg" alt="An image of me."/>
+            <figcaption>Me holding some wood.</figcaption>
+        </figure>
+        <h1>CS 142 Sitemap - Nick Brown</h1>
+        <a href="tryme.php">tryme.php</a>
+        <p> Here is a link to my <a href="https://gitlab.uvm.edu/Nicholas.Brown.1" target="_blank">git repository</a>.</p>
+        <a href="https://nbrown9.w3.uvm.edu/cs142/sitemap.xml" target="_blank">XML Sitemap</a>
+        <ul>
+            <li>Lab 1 - Setting Up 
+                <ul>
+                    <li><a href="live-lab1">Live lab</a> (CS 008 Final Project)</li>
+                    <li><a href="live-lab1/data/repo.pdf">Git repository pdf</a>.</li>
+                    <li>Print to pdf didn't work very well, so here is a <a href="live-lab1/images/repo.PNG">screenshot</a> of my git repository.</li>
+                    <li><a href="live-lab1/images/gitignore.PNG">Screenshot of .gitignore</a></li>
+                    <li><a href="live-lab1/images/userini.PNG">Screenshot of .user.ini</a></li>
+                </ul>
+            </li>
+            <li>Lab 2 - Style a Form
+                <ul>
+                    <li>The XML sitemap is linked at the top of this page.</li>
+                    <li><a href="live-lab1/form.php">Styled Form</a>.</li>
+                    <li><a href="https://nbrown9.w3.uvm.edu/cs008/FinalProject/form.php">CS008 Form (For reference)</a>.</li>
+                    <li><a href="live-lab1/form.css">form.css</a>.</li>
+                    <li><a href="live-lab1/data/repo2.pdf">Git Repo PDF</a>.</li>
+                </ul>
+            </li>
+            <li>Lab 3 - Git with a group
+                <ul>
+                    <li><a href="live-lab3/index.php">Merge Conflict Test Page</a>.</li>
+                    <li><a href="live-lab3/gitcommits.pdf">Git Repository PDF</a>.</li>
+                    <li><a href="live-lab3/Conflicts.PNG">Screenshot of the Merge Conflict</a>.</li>
+                </ul>
+            </li>
+            <li>Usability Test
+                <ul>
+                    <li><a href="live-usability-test/index.php">Usability Test Page</a>.</li>
+                    <li><a href="live-usability-test/documents/testScript.pdf">Test Script</a>.</li>
+                    <li><a href="live-usability-test/documents/Questions.pdf">Questions.pdf</a>.</li>
+                    <li><a href="live-usability-test/documents/commits.pdf">Git Commits</a></li>
+                </ul>
+            </li>
+
+            <li>Lab 7 - Responsive Menu
+                <ul>
+                    <li><a href="live-lab7/index.php">Click Here</a>.</li>
+                    <li><a href="live-lab7/data/repo7.pdf">Git Commits</a></li>
+                </ul>
+            </li>
+
+            <li>Style a Resume
+                <ul>
+                    <li><a href="live-resume/resume.php">Click Here</a>.</li>
+                    <li><a href="live-resume/repo.pdf">Git Commits</a></li>
+                </ul>
+            </li>
+
+            <li>Restyle CS Fair
+                <ul>
+                    <li><a href="live-csfair">Live Index</a></li>
+                    <li><a href="live-csfair/index-2.php">Live Index-2</a></li>
+                    <li><a href="live-csfair/how-it-went.php">how-it-went.php (Summary)</a></li>
+                    <li><a href="csfair-repo.pdf">Git Repo PDF</a></li>
+                </ul>
+            </li>
+
+            <li>Restyle a Table
+                <ul>
+                    <li><a href="live-table/table.php">Click Here</a>.</li>
+                    <li><a href="live-table/repo.pdf">Git Repository PDF</a>.</li>
+                </ul>
+            </li>
+
+            <li>Final Project
+                <ul>
+                    <li><a href="dev-final/">Dev Site</a>.</li>
+                    <li><a href="live-final/">Live Site</a>.</li>
+                    <li><a href="live-final/spec.pdf">Project Specification</a>.</li>
+                </ul>
+
+            </li>
+
+        </ul>
+    </body>
+</html>
